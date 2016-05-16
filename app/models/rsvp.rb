8b62e0e5a6691130296e96e60043f0ac45fc1935
Brett Grigsby <brett@theflyfisher.com>
2016-05-15 20:23:47 -0600

    hahahah

--- app/models/rsvp.rb
+++ app/models/rsvp.rb
@@ -1,8 +1,25 @@
 class Rsvp < ActiveRecord::Base
   has_many :guests
 
+  def self.submitted
+    all.find_all { |rsvp| rsvp.status == "submitted" }
+  end
+
+  def self.not_submitted
+    all.find_all { |rsvp| rsvp.status == "not-submitted" }
+  end
+
   def submitted_class
     submitted ? "submitted" : "not-submitted"
+  end
+
+  def status
+    results = guests.map(&:wedding).uniq
+    if results == [nil]
+      "not-submitted"
+    else
+      "submitted"
+    end
   end
 
 end
